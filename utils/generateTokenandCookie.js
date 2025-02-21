ba592b22ca65ced145cc3ee0e69cf3b99d757bea
mehul79 <mehulrgupta@gmail.com>
2025-02-21 18:05:23 +0530

    /testing-cookies-2

--- utils/generateTokenandCookie.js
+++ utils/generateTokenandCookie.js
@@ -9,8 +9,8 @@ export const generateTokenandCookie = (res, userId)=>{
 
     res.cookie("token", token, {
         httpOnly: true,
-        secure: true,
-        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",  
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", 
         path: "/",
         maxAge: 7*24*60*60*1000,
     })
