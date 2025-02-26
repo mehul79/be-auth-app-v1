6a5e9e6fe1d3a4cce7a3ca79ccd9ff9a773e7025
mehul79 <mehulrgupta@gmail.com>
2025-02-26 17:05:43 +0530

    major changes

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB = async ()=>{
-    try{
-        await mongoose.connect(process.env.MONOGO_URI);
-        console.log("MONGO DB Connected");
-    }catch(e){
-        console.log("Error while connected to db", e);
-        process.exit(1); 
+export const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
+        serverSelectionTimeoutMS: 5000, // Add timeout
+        connectTimeoutMS: 10000
+      });
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+      console.error(`Error: ${error.message}`);
+      process.exit(1);
     }
-}
+  };
