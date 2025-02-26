f5946a003130eb6b9b2887e3fd5256673e0c08a0
mehul79 <mehulrgupta@gmail.com>
2025-02-26 18:14:45 +0530

    fix: correct typo in MONGO_URI variable in connectDB function

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(process.env.MONGO_URI, {
+      const conn = await mongoose.connect(process.env.MONOGO_URI, {
         serverSelectionTimeoutMS: 5000, // Add timeout
         connectTimeoutMS: 10000
       });
