41849a27b3108c9ae0056def0c6b1fb254008a28
mehul79 <mehulrgupta@gmail.com>
2025-02-26 14:10:40 +0530

    feat: add localhost to CORS allowed origins for local development

--- index.js
+++ index.js
@@ -13,23 +13,26 @@ const PORT = process.env.PORT;
 app.use(express.json()); // Parse JSON bodies
 app.use(cookieParser()); // Parse cookies
 
-
-
 // app.options("*", cors());  //if you want to send a pre flight request
-const allowedOrigins = ['https://fe-auth-app-v1.vercel.app'];
-app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin (e.g., non-browser requests)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+const allowedOrigins = [
+  "https://fe-auth-app-v1.vercel.app",
+  "http://localhost:3000",
+];
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (e.g., non-browser requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 // Routes
 app.use("/api/auth", authroutes);
